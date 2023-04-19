b6a8b09dbe2b9ca9b3ad94bf6b9b744a63b2b8e7
niraj-knoldus-1331 <niraj.kumar@knoldus.com>
2023-04-19 20:40:39 +0530

    Added workflow

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,3 +1,27 @@
 mvn clean package
 docker build -t us.gcr.io/pharmacydeda/pharmacy-deda .
 docker push us.gcr.io/pharmacydeda/pharmacy-deda
+
+    steps:
+        - name: Checkout
+          uses: actions/checkout@v1
+
+        # Setup gcloud CLI
+        - uses: GoogleCloudPlatform/github-actions/setup-gcloud@master
+          with:
+              version: '275.0.0'
+              service_account_email: ${{ secrets.SA_EMAIL }}
+              service_account_key: ${{ secrets.GOOGLE_APPLICATION_CREDENTIALS}}
+
+            # Configure gcloud CLI
+            - name: gcloud Set up
+              run: |
+                  gcloud config set project $PROJECT_ID
+            # Build and push image to Google Container Registry
+            - name: Build
+              run: |
+                  gcloud builds submit -t gcr.io/$PROJECT_ID/$SERVICE_NAME:latest
+            # Deploy image to Cloud Run
+            - name: Deploy
+              run: |
+                  gcloud dataflow flex-template build gs://pharamacy-deda/template/run_template_new.json --image gcr.io/$PROJECT_ID/$SERVICE_NAME:latest --sdk-language JAVA
