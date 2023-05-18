cb819baaff6aa4946dee4d9d0f94e15a1c37f09d
Ankit Mogha <76722619+ankit-mogha@users.noreply.github.com>
2023-05-19 01:21:51 +0530

    Updated Github Actions (#5)
    Co-authored-by: niraj-knoldus-1331 <niraj.kumar@knoldus.com>

--- scripts/run_dataflow_job.bash
+++ scripts/run_dataflow_job.bash
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
-set -e
-set -o pipefail
-set -u
-
 echo "#######Run the Dataflow Flex Template pipeline"
 
 gcloud dataflow flex-template run "testjob-$(date +%Y%m%d-%H%M%S)" \
-  --template-file-gcs-location=gs://pharamacy-deda/template/run_template.json \
-  --worker-region=us-east4 \
-  --worker-machine-type=e2-medium \
-  --region=us-east4 \
-  --service-account-email=dataflow-run@de-da-ml.iam.gserviceaccount.com \
+  --template-file-gcs-location=gs://pharamacy-deda-poc/templates/run_template.json \
+  --worker-region=us-east1 \
+  --region=us-east1 \
+  --service-account-email=dataflow-sa@de-da-poc.iam.gserviceaccount.com \
   --num-workers=1 \
-  --parameters gcpProject=de-da-ml \
-  --parameters tempLocation=gs://pharamacy-deda/staging_location \
-  --parameters stagingLocation=gs://pharamacy-deda/temp_location
+  --parameters gcpProject=de-da-poc \
+  --parameters tempLocation=gs://pharamacy-deda-poc/staging_location \
+  --parameters stagingLocation=gs://pharamacy-deda-poc/temp_location
