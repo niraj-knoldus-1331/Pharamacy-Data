7620b1922f97467dbb4ce5d606640cfcdcd0e123
niraj-knoldus-1331 <niraj.kumar@knoldus.com>
2023-05-25 13:25:59 +0530

    Added validate records

--- src/main/java/com/knoldus/pharmacy/utils/Utility.java
+++ src/main/java/com/knoldus/pharmacy/utils/Utility.java
@@ -1,5 +1,6 @@
 package com.knoldus.pharmacy.utils;
 
+import com.google.api.services.bigquery.model.TableRow;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.Storage;
@@ -7,9 +8,10 @@ import com.knoldus.pharmacy.transformations.EncryptPIIColumns;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Instant;
 import java.util.*;
 
-import static com.knoldus.pharmacy.utils.BeamPipelineConstants.MEDICATION_FORM_TYPE;
+import static com.knoldus.pharmacy.utils.BeamPipelineConstants.*;
 
 public class Utility {
     static final Logger logger = LoggerFactory.getLogger(EncryptPIIColumns.class);
@@ -19,5 +21,15 @@ public class Utility {
         String piiColumnsPath = "pii/" + formName + "_" + version + ".txt";
         Optional<Blob> blob = Optional.ofNullable(gcsClient.get(BlobId.of(bucketName, piiColumnsPath)));
         return blob.map(value -> List.of(new String(value.getContent()).strip().split(","))).orElseGet(List::of);
+    }
+
+    public static TableRow extractDeadLetterEvent(String message, UUID uuid, Exception ex) {
+        logger.info("Extracting Dead Letter Event");
+        TableRow invalidRow = new TableRow();
+        invalidRow.set(MESSAGE_COLUMN, message);
+        invalidRow.set(EXCEPTION_MESSAGE_COLUMN, ex.getMessage());
+        invalidRow.set(GUID_COLUMN, uuid.toString());
+        invalidRow.set(INSERTION_TIME_STAMP, Instant.now().toString());
+        return invalidRow;
     }
 }
